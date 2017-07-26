1ad45083c3b34dcaac0776deef07c734452ea178
hurongyang <320654243@qq.com>
2017-07-26 22:37:39 +0800

    2017-07-26（2）

--- s14/黑马/面向对象2/13-类属性-实例属性.py
+++ s14/黑马/面向对象2/13-类属性-实例属性.py
@@ -1,10 +1,10 @@
-class Tool(object):
+class Tool(object):#Python中类也是一个对象,叫类对象（包含类属性，类方法）
     def __init__(self, new_name):
         self.name = new_name
 
 
 num = 0
-tool1 = Tool("铁锹")
+tool1 = Tool("铁锹")#实例化的对象称为实例对象（包含实例属性，实例方法）
 num+=1
 print(num)
 tool2 = Tool("工兵铲")
