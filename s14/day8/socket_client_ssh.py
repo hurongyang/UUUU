5c39b6d238599a21ae9c3ce2715443aa00edca82
hurongyang <320654243@qq.com>
2017-08-03 22:49:43 +0800

    2017-08-03（3）

--- s14/day8/socket_client_ssh.py
+++ s14/day8/socket_client_ssh.py
@@ -2,26 +2,27 @@ __author = "Hu Rongyang"
 import socket
 client = socket.socket()
 
+#client.connect(('192.168.16.200',9999))
 client.connect(('localhost',9999))
 
 while True:
-    cmd = input("请输入命令：").strip()
+    cmd = input("请输入命令:").strip()
     if len(cmd) == 0:
         continue
-
     client.send(cmd.encode("utf-8"))
-    cmd_res_size = client.recv(1024)#接收命令结果的长度
-    print("命令结果大小：",cmd_res_size)
+    cmd_res_size = client.recv(1024) ##接受命令结果的长度
+    print("命令结果大小:",cmd_res_size)
+    client.send("准备好接收了，loser可以发了".encode("utf-8"))
 
     received_size = 0
-    while received_size < int(cmd_res_size.decode()):
+    received_data = b''
+    while received_size < int(cmd_res_size.decode()) :
         data = client.recv(1024)
-        received_size += len(data)#每次收到的有可能小于1024，所以必须用len()判断
-        #print(data.decode("utf-8"))
-        print(received_size)
+        received_size += len(data) #每次收到的有可能小于1024，所以必须用len判断
+        #print(data.decode())
+        received_data += data
     else:
-        print("cmd res recived done。。。。",received_size)
-    #print(cmd_res.decode("utf-8"))
-
+        print("cmd res receive done...",received_size)
+        print(received_data.decode())
 
 client.close()
--- s14/day8/socket_server_ssh.py
+++ s14/day8/socket_server_ssh.py
@@ -1,25 +1,30 @@
 __author = "Hu Rongyang"
-import socket,os
+import socket ,os,time
 server = socket.socket()
-server.bind(('localhost',9999))
+server.bind(('localhost',9999) )
 
 server.listen()
 
 while True:
-    conn,addr = server.accept()
+    conn, addr = server.accept()
     print("new conn:",addr)
     while True:
-        print("等待新指令。。。。")
+        print("等待新指令")
         data = conn.recv(1024)
         if not data:
-            print("客户端已经断开")
+            print("客户端已断开")
             break
-        print("执行指令：",data.decode())
-        cmd_res = os.popen(data.decode()).read()#执行命令
-        print("before send:",len(cmd_res))
-        if len(cmd_res) == 0:
-            cmd_res = "cmd has no output....."
-        conn.send( str(len(cmd_res.encode())).encode("utf-8") )#先发大小给客户端
-        conn.send(cmd_res.encode("utf-8"))#发送执行结果
+        print("执行指令:",data.decode())
+        cmd_res = os.popen(data.decode()).read() #接受字符串，执行结果也是字符串
+        print("before send",len(cmd_res))
+        if len(cmd_res) ==0:
+            cmd_res = "cmd has no output..."
+
+        conn.send( str(len(cmd_res.encode())).encode("utf-8")    )     #先发大小给客户端
+        client_ack = conn.recv(1024)#wait client to confirm   ===========》 粘包的解决办法
+        print("ack from client：",client_ack)
+        conn.send(cmd_res.encode("utf-8"))
+
         print("send done")
+
 server.close()
